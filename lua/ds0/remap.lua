131a8d1de2e56f34be259871044046924ff70a34
Dave Shanahan <hello@daveshanahan.com>
2024-06-17 11:17:14 +0100

    annotations for remaps

--- lua/ds0/remap.lua
+++ lua/ds0/remap.lua
@@ -1,14 +1,17 @@
 vim.g.mapleader = " "
 
+-- Move window
 vim.keymap.set("n", "<C-h>", "<C-W>h")
 vim.keymap.set("n", "<C-j>", "<C-W>j")
 vim.keymap.set("n", "<C-k>", "<C-W>k")
 vim.keymap.set("n", "<C-l>", "<C-W>l")
 
+-- exit insert mode
 vim.keymap.set("i", "jk", "<Esc>")
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Move selection down and up
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
@@ -21,7 +24,9 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- TreeNerd
 vim.keymap.set("n", "<C-n>", vim.cmd.NvimTreeFindFile)
 vim.keymap.set("n", "<C-m>", vim.cmd.NvimTreeToggle)
 
+-- Git Blame
 vim.keymap.set("n", "<leader>gb", ":GitBlameOpenCommitURL<CR>")
