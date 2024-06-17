850f27cfbee0e19369bf5c46f382672f4e580180
Dave Shanahan <hello@daveshanahan.com>
2024-06-17 13:55:23 +0100

    Updated to use tokyo night day theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,1 +1,2 @@
-vim.cmd("colorscheme kanagawa-wave")
+-- vim.cmd("colorscheme kanagawa-lotus")
+vim.cmd[[colorscheme tokyonight-day]]
--- lua/ds0/packer.lua
+++ lua/ds0/packer.lua
@@ -8,13 +8,13 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
 
     use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.2',
-      -- or                            , branch = '0.1.x',
-      requires = { {'nvim-lua/plenary.nvim'} }
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        requires = { {'nvim-lua/plenary.nvim'} }
     }
 
     use { "ellisonleao/gruvbox.nvim" }
     use "rebelot/kanagawa.nvim"
+    use "folke/tokyonight.nvim"
 
     use {
         'nvim-treesitter/nvim-treesitter',
